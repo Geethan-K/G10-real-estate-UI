7beeb8a0bd3b869ee5cffd799005805eab788e1b
unknown <mohan.enixs@gmail.com>
2024-08-07 01:41:10 +0530

    moved layout contents to home page

--- react-estate-ui/src/routes/homePage/homePage.jsx
+++ react-estate-ui/src/routes/homePage/homePage.jsx
@@ -1,7 +1,15 @@
 import SearchBar from "../../components/searchBar/SearchBar";
-import { useState, useContext,useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../../context/AuthContext";
-import { motion, useAnimation } from 'framer-motion'
+import { motion, useAnimation } from 'framer-motion';
+import Featured from "../../components/featured/featured";
+import SmoothScroll from "../../components/smoothScroll/smoothScroll";
+import Section1 from "../../components/sections/section1/section";
+import Section from "../../components/scroll-components/section1/section1";
+import Section2 from "../../components/sections/section2/section";
+import Section3 from "../../components/sections/section3/section";
+import PropertyList from "../../components/property-list/propertyList";
+import FeaturedProperties from "../../components/featured-properties/featuredProperties";
 import "./homePage.scss";
 
 
@@ -41,54 +49,78 @@ function HomePage() {
     hidden: { opacity: 0 }
   }
   return (
-    <div className="homePage" onClick={() => handleVisibStatus(false)}>
-      <motion.div variants={contentVariants} animate={"visible"} className="box textContainer">
-        <div className="wrapper">
-          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-            explicabo suscipit cum eius, iure est nulla animi consequatur
-            facilis id pariatur fugit quos laudantium temporibus dolor ea
-            repellat provident impedit!
-          </p>
-          <SearchBar popupsVisibility={popupsVisibility} handleVisibStatus={handleVisibStatus} />
-          <div className="boxes">
-            <div className="box">
-              <h1>16+</h1>
-              <h2>Years of Experience</h2>
-            </div>
-            <div className="box">
-              <h1>200</h1>
-              <h2>Award Gained</h2>
-            </div>
-            <div className="box">
-              <h1>2000+</h1>
-              <h2>Property Ready</h2>
+    <div>
+      <div className="homePage" onClick={() => handleVisibStatus(false)}>
+        <motion.div variants={contentVariants} animate={"visible"} className="box textContainer">
+          <div className="wrapper">
+            <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+              explicabo suscipit cum eius, iure est nulla animi consequatur
+              facilis id pariatur fugit quos laudantium temporibus dolor ea
+              repellat provident impedit!
+            </p>
+            <SearchBar popupsVisibility={popupsVisibility} handleVisibStatus={handleVisibStatus} />
+            <div className="boxes">
+              <div className="box">
+                <h1>16+</h1>
+                <h2>Years of Experience</h2>
+              </div>
+              <div className="box">
+                <h1>200</h1>
+                <h2>Award Gained</h2>
+              </div>
+              <div className="box">
+                <h1>2000+</h1>
+                <h2>Property Ready</h2>
+              </div>
             </div>
           </div>
-        </div>
-      </motion.div>
-      <motion.div
-        className="box imgContainer"
-        variants={imgVariants}
-        animate={"visible"}
-      >
-        {/* <img src="/bg.png" alt="" /> */}
-        {
-          images.map((src, index) => (
-            <motion.img
-              key={index}
-              src={src}
-              alt={`Slide ${index}`}
-              
-              initial={{ opacity: 0 }}
-              animate={index === currentIndex ? controls : { opacity: 0 }}
-            />
-          ))
-        }
-      </motion.div>
+        </motion.div>
+        <motion.div
+          className="box imgContainer"
+          variants={imgVariants}
+          animate={"visible"}
+        >
+          {/* <img src="/bg.png" alt="" /> */}
+          {
+            images.map((src, index) => (
+              <motion.img
+                key={index}
+                src={src}
+                alt={`Slide ${index}`}
 
+                initial={{ opacity: 0 }}
+                animate={index === currentIndex ? controls : { opacity: 0 }}
+              />
+            ))
+          }
+        </motion.div>
+
+      </div>
+      <div>
+        <div className="body-content">
+          <SmoothScroll />
+        </div>
+        <div className="body-content">
+          <Featured />
+        </div>
+        <div className="body-content">
+          <Section2 />
+        </div>
+        <div className="body-content">
+          <PropertyList />
+        </div>
+        <div className="body-content">
+          <Section3 />
+        </div>
+        <div className="body-content">
+          <FeaturedProperties />
+        </div>
+      </div>
     </div>
+
+
   );
 }
 
--- react-estate-ui/src/routes/layout/layout.jsx
+++ react-estate-ui/src/routes/layout/layout.jsx
@@ -4,39 +4,32 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { Canvas } from '@react-three/fiber';
-import Featured from "../../components/featured/featured";
-import SmoothScroll from "../../components/smoothScroll/smoothScroll";
-import Section1 from "../../components/sections/section1/section";
-import Section from "../../components/scroll-components/section1/section1";
-import Section2 from "../../components/sections/section2/section";
-import Section3 from "../../components/sections/section3/section";
-import PropertyList from "../../components/property-list/propertyList";
-import FeaturedProperties from "../../components/featured-properties/featuredProperties";
+
 
 function Layout() {
-  useEffect(() => {
-    let currentIndex = 0;
-    const sections = document.querySelectorAll('.body-content');
-    const handleScroll = (event) => {
-      const deltaY = event.deltaY;
+  // useEffect(() => {
+  //   let currentIndex = 0;
+  //   const sections = document.querySelectorAll('.body-content');
+  //   const handleScroll = (event) => {
+  //     const deltaY = event.deltaY;
 
-      if (deltaY > 0 && currentIndex < sections.length - 1) {
-        // Scroll down
-        currentIndex++;
-        sections[currentIndex].scrollIntoView({ behavior: 'smooth' });
-      } else if (deltaY < 0 && currentIndex > 0) {
-        // Scroll up
-        currentIndex--;
-        sections[currentIndex].scrollIntoView({ behavior: 'smooth' });
-      }
-    };
+  //     if (deltaY > 0 && currentIndex < sections.length - 1) {
+  //       // Scroll down
+  //       currentIndex++;
+  //       sections[currentIndex].scrollIntoView({ behavior: 'smooth' });
+  //     } else if (deltaY < 0 && currentIndex > 0) {
+  //       // Scroll up
+  //       currentIndex--;
+  //       sections[currentIndex].scrollIntoView({ behavior: 'smooth' });
+  //     }
+  //   };
 
-    window.addEventListener('wheel', handleScroll);
+  //   window.addEventListener('wheel', handleScroll);
 
-    return () => {
-      window.removeEventListener('wheel', handleScroll);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener('wheel', handleScroll);
+  //   };
+  // }, []);
 
   return (
     <div className="layout">
@@ -46,24 +39,6 @@ function Layout() {
         <div className="content" >
           <Outlet />
         </div>
-        <div className="body-content">
-          <SmoothScroll />
-        </div>
-        <div className="body-content">
-        <Featured />
-        </div>
-        <div className="body-content">
-        <Section2 />
-        </div>
-        <div className="body-content">
-        <PropertyList />
-        </div>
-        <div className="body-content">
-        <Section3 />
-        </div>
-        <div className="body-content">
-        <FeaturedProperties />
-        </div>  
     </div>
   );
 }
